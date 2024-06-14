d8558b3ecf75c371a4ea01ca01ec1797c29d8c94
aaronalex15 <aaronalex1230@gmail.com>
2024-06-14 11:41:32 -0700

    added config

---
+++ config.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from sqlalchemy import MetaData
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_session import Session
+from os import environ
+import os
+import stripe
+from dotenv import load_dotenv
+
+# Instantiate app, set attributes
+load_dotenv()
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///GetFitted.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.secret_key = os.environ.get("SESSION_SECRET", "default_secret_key")
+app.config["SESSION_TYPE"] = "sqlalchemy"
+
+stripe_keys = {
+    "secret_key": os.environ["STRIPE_SECRET_KEY"],
+    "publishable_key": os.environ["STRIPE_PUBLISHABLE_KEY"],
+}
+
+stripe.api_key = stripe_keys["secret_key"]
+
+db = SQLAlchemy(app)
+app.config["SESSION_SQLALCHEMY"] = db
+
+migrate = Migrate(app, db)
+api = Api(app)
+ma = Marshmallow(app)
+session = Session(app)
+flask_bcrypt = Bcrypt(app)
